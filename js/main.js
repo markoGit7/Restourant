58645ee1eef644a51606e54b5eebd75f09d0cbaf
Marko Mijakovski <m.mijakovski07@gmail.com>
2024-03-18 13:05:42 +0100

    made inbox for the cart

--- js/main.js
+++ js/main.js
@@ -2,5 +2,16 @@ const respnav = document.getElementById("nav");
 const visible = document.querySelector(".regular");
 
 respnav.addEventListener("click", function(){ 
-    visible.classList.add("active");
+    respnav.classList.toggle("active");
+    visible.classList.toggle("active");
+});
+
+/* Cart inbox activation js*/
+
+const cart = document.querySelector(".cart-button");
+const inbox = document.querySelector(".inbox");
+
+cart.addEventListener("click", function(){ 
+    cart.classList.toggle("active");
+    inbox.classList.toggle("active");
 });
